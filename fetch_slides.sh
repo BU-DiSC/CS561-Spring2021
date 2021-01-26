62cd1308693d7ec24ba806ec942d5a8e2d228aca
Manos Athanassoulis <manos.athanassoulis@gmail.com>
2021-01-25 19:35:32 -0500

    cleaning index and scripting, on: fetch_slides.sh index.html

--- fetch_slides.sh
+++ fetch_slides.sh
@@ -1,1 +1,10 @@
-cp ../../../../Classes/CS591/Slides/*.pdf ./slides
+#!/bin/bash
+
+cp ~/Dropbox/Professional/Professorship/0_CLASSES/CS561-Spring2021/Slides-PDF/*.pdf ./slides
+
+git pull
+git add slides/*pdf
+git commit -m "adding new slides"
+git push
+
+
